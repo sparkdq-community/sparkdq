4c0c936981849684fb22e7f948bcac860a650d03
ba33812 <marcel.kennert@gmx.de>
2025-05-17 17:25:09 +0200

    feat(check): add DistinctRatioCheck to validate column cardinality

--- sparkdq/checks/__init__.py
+++ sparkdq/checks/__init__.py
@@ -6,6 +6,7 @@ from .aggregate.count_checks.count_max_check import RowCountMaxCheckConfig
 from .aggregate.count_checks.count_min_check import RowCountMinCheckConfig
 from .aggregate.schema_checks.column_presence_check import ColumnPresenceCheckConfig
 from .aggregate.schema_checks.schema_check import SchemaCheckConfig
+from .aggregate.uniqueness_checks.distinct_ratio_check import DistinctRatioCheckConfig
 from .aggregate.uniqueness_checks.unique_ratio_check import UniqueRatioCheckConfig
 from .aggregate.uniqueness_checks.unique_rows_check import UniqueRowsCheckConfig
 from .row_level.columns_comparison_checks.column_less_than import ColumnLessThanCheckConfig
@@ -58,4 +59,5 @@ __all__ = [
     "UniqueRatioCheckConfig",
     "CompletenessRatioCheckConfig",
     "ColumnsAreCompleteCheckConfig",
+    "DistinctRatioCheckConfig",
 ]
